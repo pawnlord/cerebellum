c0b97da99bd8f6908dd3ddec6d70a94644d87f56
pawnlord <scruffduff321@gmail.com>
2022-02-13 14:47:13 -0500

    Create test environment for assembler

--- assembler/assembler.h
+++ assembler/assembler.h
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include "coff_file.h"
+
 
 class AsmObject {
+    public:
+    AsmObject() : rt(false) {}
+
+    RelocationTable rt;
+    void add_op_imm32();
     std::vector<char> code;
-    void add_opp_imm32()
-}
+};
