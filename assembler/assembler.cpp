e843dde4ba88bcea22ede95cda92de1b7de1b2fd
pawnlord <scruffduff321@gmail.com>
2022-02-19 00:31:58 -0500

    Add modrm struct

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -11,13 +11,19 @@ void AsmObject::add_op_imm(uchar op, std::string name, int type, int size){
     rt.add_future_relocation(code.size(), name, type);
     push_back_number(code, 0, size);
 }
-void AsmObject::add_op_rm(uchar op, uint rmbyte, int extra, uint size){
+void AsmObject::add_op_rm(uchar op, modrm_t rm, int extra, uint size){
     code.push_back(op);
+    uchar rmbyte = rm.rm;
+    rmbyte += rm.reg << 3;
+    rmbyte += rm.mod << 6;
     code.push_back(rmbyte);
     push_back_number(code, extra, size);
 }
-void AsmObject::add_op_rm(uchar op, uint rmbyte, std::string name, int type, int size){
+void AsmObject::add_op_rm(uchar op, modrm_t rm, std::string name, int type, int size){
     code.push_back(op);
+    uchar rmbyte = rm.rm;
+    rmbyte += rm.reg << 3;
+    rmbyte += rm.mod << 6;
     code.push_back(rmbyte);
     rt.add_future_relocation(code.size(), name, type);
     push_back_number(code, 0, size);
--- assembler/assembler.h
+++ assembler/assembler.h
@@ -10,6 +10,12 @@ typedef enum {
     EAX = 0, ECX, EDX, EBX, ESP, EBP, ESI, EDI
 } register_e;
 
+typedef struct {
+    uchar mod;
+    uchar rm;
+    register_e reg;
+} modrm_t;
+
 class AsmObject {
     public:
     AsmObject() : rt(false) {}
@@ -17,8 +23,8 @@ class AsmObject {
     RelocationTable rt;
     void add_op_imm(uchar op, int data, int size);
     void add_op_imm(uchar op, std::string name, int type, int size);
-    void add_op_rm(uchar op, uint rmbyte, int extra, uint size);
-    void add_op_rm(uchar op, uint rmbyte, std::string name, int type, int size);
+    void add_op_rm(uchar op, modrm_t rm, int extra, uint size);
+    void add_op_rm(uchar op, modrm_t rm, std::string name, int type, int size);
     void add_op_plusr(uchar op, register_e reg);
     void add_byte(uchar op); // for anything we dont include
     std::vector<uchar> code;
