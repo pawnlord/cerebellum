146a1b188a9e0b58c7e14166a8039e4036052b03
pawnlord <scruffduff321@gmail.com>
2022-02-19 22:13:35 -0500

    add structs

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -2,31 +2,45 @@
 
 
 
-void AsmObject::add_op_imm(uchar op, int data, int size){
+void AsmObject::add_op_imm(uchar op, arg_t args){
     code.push_back(op);
-    push_back_number(code, data, size);
+    push_back_number(code, args.data, args.datasz);
 }
-void AsmObject::add_op_imm(uchar op, std::string name, int type, int size){
+void AsmObject::add_op_imm(uchar op, std::string name, int type, arg_t arg){
     code.push_back(op);
     rt.add_future_relocation(code.size(), name, type);
-    push_back_number(code, 0, size);
+    push_back_number(code, 0, arg.datasz);
 }
-void AsmObject::add_op_rm(uchar op, modrm_t rm, int extra, uint size){
+void AsmObject::add_op_rm(uchar op, modrm_t rm, arg_t arg){
     code.push_back(op);
     uchar rmbyte = rm.rm;
     rmbyte += rm.reg << 3;
     rmbyte += rm.mod << 6;
     code.push_back(rmbyte);
-    push_back_number(code, extra, size);
+    push_back_number(code, arg.disp, arg.dispsz);
+    push_back_number(code, arg.data, arg.datasz);
 }
-void AsmObject::add_op_rm(uchar op, modrm_t rm, std::string name, int type, int size){
+void AsmObject::add_op_rm(uchar op, modrm_t rm, sib_t sib, arg_t arg){
+    code.push_back(op);
+    uchar rmbyte = rm.rm;
+    rmbyte += rm.reg << 3;
+    rmbyte += rm.mod << 6;
+    uchar sibbyte = sib.base;
+    sibbyte += sib.index << 3;
+    sibbyte += sib.ss << 6;
+    code.push_back(rmbyte);
+    code.push_back(sibbyte);
+    push_back_number(code, arg.disp, arg.dispsz);
+    push_back_number(code, arg.data, arg.datasz);
+}
+void AsmObject::add_op_rm(uchar op, modrm_t rm, std::string name, int type, arg_t arg){
     code.push_back(op);
     uchar rmbyte = rm.rm;
     rmbyte += rm.reg << 3;
     rmbyte += rm.mod << 6;
     code.push_back(rmbyte);
     rt.add_future_relocation(code.size(), name, type);
-    push_back_number(code, 0, size);
+    push_back_number(code, 0, arg.datasz);
 }
 void AsmObject::add_byte(uchar op){
     code.push_back(op);
--- assembler/assembler.h
+++ assembler/assembler.h
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <vector>
 #include "coff_file.h"
+#define arg(data, sz) {data, sz, 0, 0}
+#define argsz(sz) {0,sz,0,0}
 
 typedef unsigned char uchar;
 typedef unsigned char uint;
@@ -16,15 +18,28 @@ typedef struct {
     register_e reg;
 } modrm_t;
 
+typedef struct {
+    uchar ss;
+    uchar index;
+    register_e base;
+} sib_t;
+
+typedef struct {
+    int data;
+    int datasz;
+    int disp;
+    int dispsz;
+} arg_t;
 class AsmObject {
     public:
     AsmObject() : rt(false) {}
 
     RelocationTable rt;
-    void add_op_imm(uchar op, int data, int size);
-    void add_op_imm(uchar op, std::string name, int type, int size);
-    void add_op_rm(uchar op, modrm_t rm, int extra, uint size);
-    void add_op_rm(uchar op, modrm_t rm, std::string name, int type, int size);
+    void add_op_imm(uchar op, arg_t args);
+    void add_op_imm(uchar op, std::string name, int type, arg_t argsz);
+    void add_op_rm(uchar op, modrm_t rm, arg_t arg);
+    void add_op_rm(uchar op, modrm_t rm, sib_t sib, arg_t arg);
+    void add_op_rm(uchar op, modrm_t rm, std::string name, int type, arg_t argsz);
     void add_op_plusr(uchar op, register_e reg);
     void add_byte(uchar op); // for anything we dont include
     std::vector<uchar> code;
