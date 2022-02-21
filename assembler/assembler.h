e4bb5af415e9b0d88bae712a95c965528059fdc9
pawnlord <scruffduff321@gmail.com>
2022-02-20 23:36:55 -0500

    Document assembler

--- assembler/assembler.h
+++ assembler/assembler.h
@@ -35,12 +35,38 @@ class AsmObject {
     AsmObject() : rt(false) {}
 
     RelocationTable rt;
+    /** 
+     * op: the assembly instruction
+     * args: the imm data to be put in the command (use: arg(data, sz))
+    */
     void add_op_imm(uchar op, arg_t args);
+    /** 
+     * name: symbol name
+     * type: redirection type (0x6 - "variable", 0x14 - "function")
+     * args: the size of the symbol (use: argsz(sz))
+    */
     void add_op_imm(uchar op, std::string name, int type, arg_t argsz);
+    
+    /** 
+     * rm: byte specifying the registers and pointers to use for arguments
+     * args: the data not specified by the rm (data and displacement)
+     *      (use: {data, sz, [disp], [dispsz]})
+    */
     void add_op_rm(uchar op, modrm_t rm, arg_t arg);
+    /** 
+     * sib: extra byte specifying multiplying a reigster (ex. [EAX*2])
+     */
     void add_op_rm(uchar op, modrm_t rm, sib_t sib, arg_t arg);
     void add_op_rm(uchar op, modrm_t rm, std::string name, int type, arg_t argsz);
+    /**
+     * uses instruction+register to make command
+     * useful for pushing registers
+     * register: assembly register to add
+    */
     void add_op_plusr(uchar op, register_e reg);
+    /**
+     * push single byte for nonspecific instructions
+    */
     void add_byte(uchar op); // for anything we dont include
     std::vector<uchar> code;
 };
